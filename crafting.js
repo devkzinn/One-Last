1087c8831967e8cd00e99d6086c8fad4a62cc2c4
Kzinn <kauafelipe18102007@gmail.com>
2026-01-28 13:14:52 -0300

    crafting.js

--- crafting.js
+++ crafting.js
@@ -2,4 +2,7 @@
     const craftingRecipes = [
         { name: 'Espada', requires: { pedrinha: 2, graveto: 1 }, result: { type: 'espada', name: 'Espada' } },
         { name: 'Machado', requires: { pedrinha: 3, graveto: 2 }, result: { type: 'machado', name: 'Machado' } },
+        { name: 'Picareta', requires: { pedrinha: 4, graveto: 2 }, result: { type: 'picareta', name: 'Picareta' } },
+        { name: 'Flecha', requires: { graveto: 1, pena: 1 }, result: { type: 'flecha', name: 'Flecha' } },
+        { name: 'Arco', requires: { graveto: 3, corda: 1 }, result: { type: 'arco', name: 'Arco' } },
     ];
